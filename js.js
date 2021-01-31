1301e6105156f6977a75e9b9f3688eb96b5fb4e8
wsf <wusfun@foxmail.com>
2021-02-01 02:56:09 +0800

    +...

---
+++ js.js
@@ -0,0 +1,38 @@
+function fun(a, b) {
+  return a + b
+}
+
+
+a = fun(1, 3)
+
+b = a + 423232323
+
+
+c = b + a
+
+console.log(c)
+
+if (c) {
+  fun()
+}
+
+a = { n: 1 }
+b = { n: 2 }
+
+a, b
+
+  ; (
+    (1), (
+      (
+        (
+          (1)))))
+
+fun
+
+valid
+
+
+fun.call
+
+log
+fun
